c3c7b03f0dc7d9cb5c2e02297eb3d41ace0da108
bkommineni <bkommineni@dons.usfca.edu>
2017-10-03 14:15:19 -0400

    Code to connect android to client:not working,not able to establish connectivity

--- DemoDataManager/app/src/main/java/com/emotionsense/demo/data/MainActivity.java
+++ DemoDataManager/app/src/main/java/com/emotionsense/demo/data/MainActivity.java
@@ -1,5 +1,8 @@
 package com.emotionsense.demo.data;
 
+import java.net.HttpURLConnection;
+import java.net.Socket;
+import java.net.URL;
 import java.util.List;
 
 import android.app.Activity;
@@ -28,24 +31,34 @@ public class MainActivity extends Activity implements DataUploadCallback
 	
 	private SubscribeThread[] subscribeThreads;
 	private SenseOnceThread[] pullThreads;
+	private HttpURLConnection conn = null;
+
+	//Client client = new Client("http://localhost:9999/data",9999);
 
 	// TODO: add push sensors you want to sense from here
 	private final int[] pushSensors = {SensorUtils.SENSOR_TYPE_PROXIMITY,
-			SensorUtils.SENSOR_TYPE_BATTERY,
+			SensorUtils.SENSOR_TYPE_BATTERY,SensorUtils.SENSOR_TYPE_PHONE_STATE
 	};
 	
 	 // TODO: add pull sensors you want to sense once from here
 	private final int[] pullSensors = {SensorUtils.SENSOR_TYPE_ACCELEROMETER,
-			 SensorUtils.SENSOR_TYPE_GYROSCOPE};
+			 SensorUtils.SENSOR_TYPE_GYROSCOPE,
+	         SensorUtils.SENSOR_TYPE_LOCATION,
+	         SensorUtils.SENSOR_TYPE_MAGNETIC_FIELD,
+			 SensorUtils.SENSOR_TYPE_STEP_COUNTER};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		//client.connect();
+
 
 		try
 		{
+			URL url = new URL("http://localhost:9999/data");
+			conn = (HttpURLConnection) url.openConnection();
 			// TODO: change this line of code to change the type of data logger
 			// Note: you shouldn't have more than one logger!
 //			logger = AsyncEncryptedDatabase.getInstance();
@@ -65,7 +78,7 @@ public class MainActivity extends Activity implements DataUploadCallback
 			pullThreads = new SenseOnceThread[pullSensors.length];
 			for (int i = 0; i < pullSensors.length; i++)
 			{
-				pullThreads[i] = new SenseOnceThread(this, sensorManager, logger, pullSensors[i]);
+				pullThreads[i] = new SenseOnceThread(this, sensorManager, logger, pullSensors[i],conn);
 				Log.d("debug",Integer.toString(pullSensors[i]));
 				System.out.println("pull sensors : " + Integer.toString(pullSensors[i]));
 				pullThreads[i].start();
@@ -177,5 +190,11 @@ public class MainActivity extends Activity implements DataUploadCallback
 				Toast.makeText(MainActivity.this, "Error transferring data", Toast.LENGTH_LONG).show();
 			}
 		});
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		//client.disconnect();
 	}
 }
--- DemoDataManager/app/src/main/java/com/emotionsense/demo/data/SenseOnceThread.java
+++ DemoDataManager/app/src/main/java/com/emotionsense/demo/data/SenseOnceThread.java
@@ -10,19 +10,29 @@ import com.ubhave.sensormanager.ESSensorManager;
 import com.ubhave.sensormanager.data.SensorData;
 import com.ubhave.sensormanager.sensors.SensorUtils;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class SenseOnceThread extends Thread
 {
 	private final int sensorType;
 	private final Activity resultScreen;
 	private final ESSensorManager sensorManager;
 	private final AbstractDataLogger logger;
+	//private Client clientSocket = null;
+	private HttpURLConnection conn = null;
 	
-	public SenseOnceThread(final Activity resultScreen, final ESSensorManager sensorManager, AbstractDataLogger logger, int sensorType)
+	public SenseOnceThread(final Activity resultScreen, final ESSensorManager sensorManager, AbstractDataLogger logger, int sensorType,
+							HttpURLConnection conn)
 	{
 		this.resultScreen = resultScreen;
 		this.sensorManager = sensorManager;
 		this.logger = logger;
 		this.sensorType = sensorType;
+		this.conn = conn;
 	}
 	
 	private void toast(final String message)
@@ -43,6 +53,37 @@ public class SenseOnceThread extends Thread
 		try
 		{
 			SensorData data = sensorManager.getDataFromSensor(sensorType);
+			//clientSocket.send("hi server!!!");
+			try {
+				conn.setDoOutput(true);
+				conn.setRequestMethod("POST");
+				conn.setRequestProperty("Content-Type", "application/json");
+
+				String input = data.toString();
+
+				OutputStream os = conn.getOutputStream();
+				os.write(input.getBytes());
+				os.flush();
+
+				if (conn.getResponseCode() != HttpURLConnection.HTTP_CREATED) {
+					throw new RuntimeException("Failed : HTTP error code : "
+							+ conn.getResponseCode());
+				}
+
+				BufferedReader br = new BufferedReader(new InputStreamReader(
+						(conn.getInputStream())));
+
+				String output;
+				System.out.println("Output from Server .... \n");
+				while ((output = br.readLine()) != null) {
+					System.out.println(output);
+				}
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+			}
+			Log.d("data",data.toString());
 			if (data != null)
 			{
 				logger.logSensorData(data);
