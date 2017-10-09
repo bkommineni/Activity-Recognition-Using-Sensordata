8e67a91ccf45070d6185d23b7286b17e38263c38
Surada Lerkpatomsak <slerkpatomsak@usfca.edu>
2017-10-09 15:19:05 -0700

    Server.js

--- server-database/importJsonFiles.js
+++
@@ -1,3 +0,0 @@
-for filename in
-DemoDataManager/DataCollected/SensorDataJson/Accelerometer;
-do mongoimport -d activityAnalyzerDB -c $filename;  done
--- server-database/readJson.js
+++
@@ -1,7 +0,0 @@
-var fs = require("fs");
-var content = fs.readFileSync("../DemoDataManager/DataCollected/SensorDataJson/Accelerometer/1506055760997.json");
-//console.log("Output Content: \n"+ content);
-
-var igor = content['userid'];
-console.log(igor);
-//var jsonStr = JSON.stringify(content);;//console.log("Userid:", jsonContent);
