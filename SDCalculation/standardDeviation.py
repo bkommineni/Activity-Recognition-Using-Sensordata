12beae0d8e938f9b3b8dd9c5d77ad3a72dd8f551
Surada Lerkpatomsak <slerkpatomsak@usfca.edu>
2017-10-31 14:23:49 -0700

    Binned Distribution

--- SDCalculation/standardDeviation.py
+++ SDCalculation/standardDeviation.py
@@ -9,8 +9,8 @@ class Stddev(object):
     def calMean(self): #Average
         return statistics.mean(self.lst)
 
-    def calMedian(self): #Median
-        return statistics.median(self.lst)
+    # def calMedian(self): #Median
+    #      return statistics.median(self.lst)
 
     def calStd(self): #Standard Deviation
         return statistics.stdev(self.lst)
@@ -21,6 +21,17 @@ class Stddev(object):
             result += math.sqrt((xi**2)+(yi**2)+(zi**2))
 
         return result/self.lst
+
+    def calBinnedDist(self): #Binned Distribution
+        rangeVal = max(self.lst)-min(self.lst)
+        binKey = []
+        for i in np.arange(rangeVal/10,rangeVal+(rangeVal/10),rangeVal/10):
+            binKey.append(i)
+
+        #bins = dict.fromkeys(binKey)
+        return binKey
+
+
 
 if __name__ == '__main__':
     with open('data/data.json') as data_file:
@@ -39,9 +50,9 @@ if __name__ == '__main__':
     print  "Standard Deviation of yAxis is " + str(yAxis.calStd())
     print  "Standard Deviation of zAxis is " + str(xAxis.calStd())
 
-    print  "\nMedian of zAxis is " + str(zAxis.calMedian())
-    print  "Median of yAxis is " + str(yAxis.calMedian())
-    print  "Median of xAxis is " + str(xAxis.calMedian())
+    # print  "\nMedian of zAxis is " + str(zAxis.calMedian())
+    # print  "Median of yAxis is " + str(yAxis.calMedian())
+    # print  "Median of xAxis is " + str(xAxis.calMedian())
 
     print  "\nMean of zAxis is " + str(zAxis.calMean())
     print  "Mean of yAxis is " + str(yAxis.calMean())
@@ -49,9 +60,9 @@ if __name__ == '__main__':
 
     print "\nAverage Resultant Acceleration is " + str(allAxis.calAvgRstAccln(xAxis_data,yAxis_data,zAxis_data))
 
-
-
-
+    print "\nBinned Distribution of zAxis is " + str(Stddev(zAxis_data).calBinnedDist());
+    print "Binned Distribution of yAxis is " + str(Stddev(yAxis_data).calBinnedDist());
+    print "Binned Distribution of xAxis is " + str(Stddev(xAxis_data).calBinnedDist());
 
 
 
