b76386d975f875ea9bc15733af7f2f677a9d3aad
Surada Lerkpatomsak <slerkpatomsak@usfca.edu>
2017-10-31 13:57:53 -0700

    mean,median,sd,Average Resultant Acceleration

--- SDCalculation/standardDeviation.py
+++ SDCalculation/standardDeviation.py
@@ -1,22 +1,28 @@
-from math import sqrt
-import json
+import json,statistics,math
+import numpy as np
 
 class Stddev(object):
 
     def __init__(self,lst):
         self.lst = lst
-        self.mean = 0
 
-    def calSum(self):
-        return sum(self.lst) / len(self.lst)
+    def calMean(self): #Average
+        return statistics.mean(self.lst)
 
-    def calStd(self):
-        self.mean = sum(self.lst) / len(self.lst)
-        variance = float(sum([(e - self.mean) ** 2 for e in self.lst])) / (len(self.lst)-1)
-        return sqrt(variance)
+    def calMedian(self): #Median
+        return statistics.median(self.lst)
+
+    def calStd(self): #Standard Deviation
+        return statistics.stdev(self.lst)
+
+    def calAvgRstAccln(self,x,y,z): #Average Resultant Acceleration
+        result = 0
+        for xi,yi,zi in zip(x,y,z):
+            result += math.sqrt((xi**2)+(yi**2)+(zi**2))
+
+        return result/self.lst
 
 if __name__ == '__main__':
-
     with open('data/data.json') as data_file:
         data = json.load(data_file)
 
@@ -24,13 +30,27 @@ if __name__ == '__main__':
     yAxis_data = data["yAxis"]
     xAxis_data = data["xAxis"]
 
-    zAxis_std = Stddev(zAxis_data)
-    yAxis_std = Stddev(yAxis_data)
-    xAxis_std = Stddev(xAxis_data)
+    zAxis = Stddev(zAxis_data)
+    yAxis = Stddev(yAxis_data)
+    xAxis = Stddev(xAxis_data)
+    allAxis = Stddev(len(xAxis_data))
 
-    print  "\nStandard Deviation of zAxis is " + str(round(zAxis_std.calStd(),2))
-    print  "Standard Deviation of yAxis is " + str(yAxis_std.calStd())
-    print  "Standard Deviation of zAxis is " + str(xAxis_std.calStd())
+    print  "\nStandard Deviation of zAxis is " + str(round(zAxis.calStd(),2))
+    print  "Standard Deviation of yAxis is " + str(yAxis.calStd())
+    print  "Standard Deviation of zAxis is " + str(xAxis.calStd())
+
+    print  "\nMedian of zAxis is " + str(zAxis.calMedian())
+    print  "Median of yAxis is " + str(yAxis.calMedian())
+    print  "Median of xAxis is " + str(xAxis.calMedian())
+
+    print  "\nMean of zAxis is " + str(zAxis.calMean())
+    print  "Mean of yAxis is " + str(yAxis.calMean())
+    print  "Mean of xAxis is " + str(xAxis.calMean())
+
+    print "\nAverage Resultant Acceleration is " + str(allAxis.calAvgRstAccln(xAxis_data,yAxis_data,zAxis_data))
+
+
+
 
 
 
