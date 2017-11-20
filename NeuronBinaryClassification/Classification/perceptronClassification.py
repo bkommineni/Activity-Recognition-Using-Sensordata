b57927e44c975642d4eab3bed99a00ee80e972a1
Surada Lerkpatomsak <slerkpatomsak@usfca.edu>
2017-11-20 09:32:29 -0800

    Graph

--- NeuronBinaryClassification/Classification/perceptronClassification.py
+++ NeuronBinaryClassification/Classification/perceptronClassification.py
@@ -13,11 +13,13 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 ppn = Perceptron(n_iter=40, eta0=0.1, random_state=1)
 ppn.fit(X_train, y_train)
 y_pred = ppn.predict(X_test)
+
 print('\nAccuracy of Accuracy Score : %.2f' % accuracy_score(y_test,y_pred))
 print('\nAccuracy of Perceptron Score: %.2f' % ppn.score(X_test,y_test))
-print ('\nConfustion Metrix')
-print (confusion_matrix(y_test,y_pred))
+print('\nConfustion Metrix')
+print(confusion_matrix(y_test,y_pred))
 print(classification_report(y_test,y_pred))
+print(ppn.coef_)
 
 
 
--- NeuronBinaryClassification/CrossValidation/K-fold.py
+++ NeuronBinaryClassification/CrossValidation/K-fold.py
@@ -2,6 +2,8 @@ from sklearn.linear_model import Perceptron
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import cross_val_score
+from sklearn.metrics import accuracy_score
+
 import numpy as np
 import pandas as pd
 
@@ -17,18 +19,20 @@ y_pred = ppn.predict(X_test)
 print 'X train size =', len(X_train)
 print 'Y train size =' , len(y_train)
 
-kfold = StratifiedKFold(n_splits=32, random_state=1).split(X_train, y_train)
-scores = cross_val_score(estimator=ppn, X=X_train, y=y_train, cv=32, n_jobs=1)
+scores = cross_val_score(estimator=ppn, X=X_train, y=y_train, cv=10, n_jobs=1)
+print ('\nCV accuracy: %.3f +/- %.3f\n' % (np.mean(scores), np.std(scores)))
 
-# scores = []
-# for k, (train, test) in enumerate(kfold):
-#     ppn.fit(X_train[train], y_train[train])
-#     score = ppn.score(X_test[test], y_test[test])
-#     scores.append(score)
-#     print ('Fold: %2d, Class dist. : %s, Acc: %.3f' % (k+1, y_train[train], score))
-#
-#
-# print ('\nCV accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
+kfold = StratifiedKFold(n_splits=10, random_state=1).split(X_train, y_train)
+scoress = []
+for k, (train, test) in enumerate(kfold):
+
+    ppn.fit(X_train[train], y_train[train])
+    y_pred = ppn.predict(X_test)
+    score = accuracy_score(y_test,y_pred)
+    scoress.append(score)
+    print ('Fold: %2d,  Acc: %.3f' % (k+1,  score))
+
+print ('\nCV accuracy: %.3f +/- %.3f' % (np.mean(scoress), np.std(scoress)))
 
 
 
