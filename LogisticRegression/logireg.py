d3bb3790404440ad29ceca8a2e95360684af7db9
Anjani Bajaj <aabajaj2@dons.usfca.edu>
2017-11-26 19:27:03 -0800

    Jupyter Notebook update and added pdf file

--- LogisticRegression/logireg.py
+++ LogisticRegression/logireg.py
@@ -7,18 +7,23 @@ import numpy as np
 
 if __name__ == '__main__':
 
-    lr = LogisticRegression(C=100.0, random_state=1)
     df_features = pd.read_csv("H:/mastersProject/activity_analyzer/LogisticRegression/Data/featuresfile.csv")
-    X_data = df_features.values[:, 1:44]
-    y_data = df_features.values[:, 44]
-    X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size = 0.33, random_state = 42)
-    probas = lr.fit(X_train, y_train)
+    X_train_data = df_features.values[:, 1:44]
+    y_train_data = df_features.values[:, 44]
+    df_features_test = pd.read_csv("H:/mastersProject/activity_analyzer/LogisticRegression/Data/featuresfile_10.csv")
+    # X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size = 0.33, random_state = 42)
+    X_test = df_features_test.values[:, 2:45]
+    y_test = df_features_test.values[:, 45]
+
+    lr = LogisticRegression(C=100.0, random_state=1)
+    lr.fit(X_train_data, y_train_data)
     predict = lr.predict(X_test)
     logisticRegScore = lr.score(X_test, y_test)
     print(lr.coef_)
     print(logisticRegScore)
+
     cm = confusion_matrix(y_test, predict, labels=["walking", "running"])
     print(cm)
 
-    classes = np.unique(y_test)
+    # classes = np.unique(y_test)
 
