1861d4d2b0d2bcd137d55a2bd567635ac7d19a8c
Surada Lerkpatomsak <slerkpatomsak@usfca.edu>
2017-12-01 00:12:29 -0800

    Graph

--- NeuronBinaryClassification/Graph/ConfusionMatrixPlot.py
+++ NeuronBinaryClassification/Graph/ConfusionMatrixPlot.py
@@ -21,12 +21,13 @@ y_pred = ppn.predict(X_test)
 confmt = confusion_matrix(y_test,y_pred)
 
 fig, ax = plt.subplots(figsize=(2.5,2.5))
-ax.matshow(confmt, cmap=plt.cm.Blues, alpha=0.3)
+ax.matshow(confmt, cmap=plt.cm.Blues)
 for i in range(confmt.shape[0]):
     for j in range(confmt.shape[1]):
         ax.text(x=j, y=i, s=confmt[i,j],va='center',ha='center')
 plt.xlabel('predicted label')
 plt.ylabel('true label')
+# plt.savefig('./images/scatter_AvgAbsDiffxy_dt_train_3_val_10.png',dpi=1000)
 plt.show()
 
 
